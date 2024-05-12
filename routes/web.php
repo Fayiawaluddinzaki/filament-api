dcdf667db71fb91e4820d908e9a285bfba22f23c
Fayiawaluddinzaki <zakiawaluddin@gmail.com>
2024-05-12 14:23:59 +0700

    Ubah Dikit

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Filament\Http\Controllers\Auth\LoginController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route untuk langsung menuju ke halaman login Filament
+// Route::get('/', [LoginController::class, 'create'])->name('filament.login');
+
